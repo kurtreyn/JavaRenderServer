13000e57dc6d721d215c2ed3c2d82bf44e1bd3db
kurtreyn <kurtreyn@yahoo.com>
2023-12-28 11:02:55 -0500

    remove cors config from main class

--- src/main/java/com/restapi/RestApiApplication.java
+++ src/main/java/com/restapi/RestApiApplication.java
@@ -2,9 +2,6 @@ package com.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -14,15 +11,5 @@ public class RestApiApplication {
 		SpringApplication.run(RestApiApplication.class, args);
 
 	}
-
-    //@Bean
-    //public WebMvcConfigurer corsConfigurer() {
-    //    return new WebMvcConfigurer() {
-    //        @Override
-    //        public void addCorsMappings(CorsRegistry registry) {
-    //            registry.addMapping("/users").allowedOrigins("http://localhost:4200");
-    //        }
-    //    };
-    //}
 
 }
